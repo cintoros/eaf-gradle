d87e31198180b1c58796ebb01e72e872d539cb99
Elias Schorr <elias.schorr@fhnw.ch>
2020-09-17 09:20:03 +0200

    add codestyle

---
+++ l01/src/main/java/ch/fhnw/eaf/rental/MovieRentalClient.java
@@ -0,0 +1,28 @@
+package ch.fhnw.eaf.rental;
+
+import ch.fhnw.eaf.rental.gui.BusinessLogic;
+import ch.fhnw.eaf.rental.gui.MovieRentalApplicationGui;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class MovieRentalClient implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(MovieRentalClient.class)
+			.headless(false) // we start a GUI and are not headless though
+			.web(WebApplicationType.NONE) // set to SERVLET if h2 console should be enabled
+			.run(args);
+	}
+
+	@Autowired
+    BusinessLogic logic;
+
+	@Override
+	public void run(String... args) throws Exception {
+		java.awt.EventQueue.invokeLater(() -> new MovieRentalApplicationGui(logic).setVisible(true));
+	}
+}
